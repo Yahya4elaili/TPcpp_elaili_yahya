fcf25b9e026cad7c3d7ed21bf09e2e57055495b5
Yahya4elaili <135591455+Yahya4elaili@users.noreply.github.com>
2024-01-12 16:01:49 +0100

    final review and implementation of KNN & KNNDouble & KnnString , (not sure of it )

--- Animal.h
+++ Animal.h
@@ -2,6 +2,8 @@
 // Created by yahya on 1/12/2024.
 //
 
+#ifndef ANIMAL_H
+#define ANIMAL_H
 
 #include <string>
 #include <iostream>
@@ -57,4 +59,5 @@ public:
     virtual void printInfo() const;
 };
 
+#endif // ANIMAL_H
 
